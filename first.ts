45d20208daca1a82bd88db63c708e5a0d0ebee98
developer-suhag <developersuhag@gmail.com>
2021-09-10 00:45:25 +0600

    type

--- first.ts
+++ first.ts
@@ -1,3 +1,13 @@
-const myNumber = 13;
+/* const myNumber = 13;
 const myName = "Suhag";
-console.log("hello from typescript", myNumber, myName);
+console.log("hello from typescript", myNumber, myName); */
+
+/* let money = 500;
+money = "taka poisha hater moila";
+money = false; */
+
+let money: number = 500;
+let honey: string = "honey is very sweet";
+let doney: boolean = false;
+
+const myName: string = "Suhag";
--- js/first.js
+++ js/first.js
@@ -1,4 +1,11 @@
 "use strict";
-const myNumber = 13;
+/* const myNumber = 13;
 const myName = "Suhag";
-console.log("hello from typescript", myNumber, myName);
+console.log("hello from typescript", myNumber, myName); */
+/* let money = 500;
+money = "taka poisha hater moila";
+money = false; */
+let money = 500;
+let honey = "honey is very sweet";
+let doney = false;
+const myName = "Suhag";
