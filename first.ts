5f468d47cbc3f9ce8c41558de5f1ec4e358153a2
developer-suhag <developersuhag@gmail.com>
2021-09-10 11:10:27 +0600

    function type

--- first.ts
+++ first.ts
@@ -11,3 +11,27 @@ let honey: string = "honey is very sweet";
 let doney: boolean = false;
 
 const myName: string = "Suhag";
+
+// function add(first: number, second: number): number {
+//   const result = first + second;
+//   return result;
+// }
+
+// const output: number = add(34, 45);
+
+function double(x: number): void {
+  console.log(2 * x);
+}
+double(2);
+
+// multipurpose
+
+/* function add(first: number | string, second: number | string): number | string {
+  const result = first + second;
+  return result;
+}
+
+const output: number = add(34, 45);
+const output1: number = add("Suhag", " Mokta");
+
+console.log(output, output1); */
--- js/first.js
+++ js/first.js
@@ -9,3 +9,22 @@ let money = 500;
 let honey = "honey is very sweet";
 let doney = false;
 const myName = "Suhag";
+// function add(first: number, second: number): number {
+//   const result = first + second;
+//   return result;
+// }
+// const output: number = add(34, 45);
+function double(x) {
+    console.log(2 * x);
+}
+double(2);
+// multipurpose
+/* function add(first: number | string, second: number | string): number | string {
+  const result = first + second;
+  return result;
+}
+
+const output: number = add(34, 45);
+const output1: number = add("Suhag", " Mokta");
+
+console.log(output, output1); */
